c9c56b60c1362182bef68c0fb39ce0fc8abc88f5
DsiahrzComputhink <damiensiah11@gmail.com>
2025-10-26 03:30:28 +0000

    question2done

--- testquestion2.js
+++ testquestion2.js
@@ -4,8 +4,10 @@ function setup(){
     new Canvas(400, 600);
    
     // code to set the gravity of the world
+    world.gravity.y = 1;
 
     // code to create a new square sprite
+    square = new Sprite(width/2, height/2, 50, 50, "dynamic")
 }
 
 function draw(){
@@ -13,4 +15,7 @@ function draw(){
 
     // code to check if mouse is pressed i.e. mouse.presses()
         // if mouse is pressed, the square sprite "flies" up
+    if (mouse.presses()) {
+        square.velocity.y = -1
+    }
 }
