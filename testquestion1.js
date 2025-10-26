5d649d5cd9d6ccaf6de1a20beaaf5761b0b9bc16
DsiahrzComputhink <damiensiah11@gmail.com>
2025-10-26 03:24:15 +0000

    question1done

--- testquestion1.js
+++ testquestion1.js
@@ -1,1 +1,22 @@
+let bird
+let background, birdImg;
 
+let birdSprite;
+// placeholder for image to be put
+
+function preload(){
+    bird = loadImage("assets/bird.png");
+    background = loadImage("assets/background.png");
+    // preloads images for usage in draw function
+}
+
+function setup(){
+    new Canvas(400, 600);
+    birdSprite = new Sprite(width/2, height/2, 34, 24, "static")
+    // Creates Placeholder sprite for image to be put in draw function
+}
+
+function draw(){
+    image(background, 0, 0, width, height);
+    birdSprite.image = image(bird, width/2, height/2, 34, 24);
+}
