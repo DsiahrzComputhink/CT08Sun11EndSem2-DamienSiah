25807e96a254694a2363cc3abf4aee7846ab7df2
DsiahrzComputhink <damiensiah11@gmail.com>
2025-10-26 03:31:52 +0000

    question1fix

--- testquestion1.js
+++ testquestion1.js
@@ -1,7 +1,7 @@
 let bird
 let background, birdImg;
 
-let birdSprite;
+//let birdSprite;
 // placeholder for image to be put
 
 function preload(){
@@ -12,11 +12,11 @@ function preload(){
 
 function setup(){
     new Canvas(400, 600);
-    birdSprite = new Sprite(width/2, height/2, 34, 24, "static")
+    //birdSprite = new Sprite(width/2, height/2, 34, 24, "static")
     // Creates Placeholder sprite for image to be put in draw function
 }
 
 function draw(){
     image(background, 0, 0, width, height);
-    birdSprite.image = image(bird, width/2, height/2, 34, 24);
+    image(bird, width/2, height/2, 34, 24);
 }
